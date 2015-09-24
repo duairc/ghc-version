5d83d7d998c57d90529705c22832d15adb70b865
Shane O'Brien <shane@duairc.com>
2015-09-24 18:58:22 +0100

    It's no longer necessary to install cabal for each GHC. You now just install cabal-head from ppa:hvr/ghc

--- build-cabal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ghc --make Setup.hs
-./Setup configure --extra-prog-path=$HOME/.cabal/bin/$(ghc-version) --bindir=$HOME/.cabal/bin/$(ghc-version) --libexecdir=$HOME/.cabal/libexec/$(ghc-version) --sysconfdir=$HOME/.cabal/etc/$(ghc-version) --user
-./Setup build
-./Setup install --user
